cd6ff6638a043386b6c1219d1b822e18a583ce72
silentstorm <mynutshang@cbt.com>
2022-06-05 16:30:52 +1000

    add doublecash

---
+++ double cash/ModMain.cs
@@ -0,0 +1,33 @@
+﻿using MelonLoader;
+using HarmonyLib;
+[assembly:MelonInfo(typeof(DoubleCash.ModMain),"DoubleCash","1.0.0","Silentstorm")]
+[assembly:MelonGame("Sayan","Apes vs Helium")]
+namespace DoubleCash{
+    public class ModMain:MelonMod{
+        private static MelonLogger.Instance mllog=null;
+        public static void Log(object thingtolog,string type="msg"){
+            switch(type){
+                case"msg":
+                    mllog.Msg(thingtolog);
+                    break;
+                case"warn":
+                    mllog.Warning(thingtolog);
+                    break;
+                 case"error":
+                    mllog.Error(thingtolog);
+                    break;
+            }
+        }
+        public override void OnApplicationStart(){
+            mllog=LoggerInstance;
+            Log("DoubleCash loaded!");
+        }
+        [HarmonyPatch(typeof(Currency),"UpdateCurrency")]
+        public class CurrencyUpdateCurrency_Patch{
+            [HarmonyPrefix]
+            public static void Prefix(ref int amount){
+                amount=amount*2;
+            }
+        }
+    }
+}
