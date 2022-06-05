303800b7d0da01066654882e21756f7990d94f32
silentstorm <mynutshang@cbt.com>
2022-06-05 14:13:58 +1000

    add template to repo

---
+++ template/ModMain.cs
@@ -0,0 +1,26 @@
+﻿using MelonLoader;
+using HarmonyLib;
+[assembly:MelonInfo(typeof(avhtemplate.ModMain),"avhtemplate","1.0.0","Silentstorm")]
+[assembly:MelonGame("Sayan","Apes vs Helium")]
+namespace avhtemplate{
+    public class ModMain:MelonMod{
+        private static MelonLogger.Instance mllog=null;
+        public static void Log(object thingtolog,string type="msg"){
+            switch(type){
+                case"msg":
+                    mllog.Msg(thingtolog);
+                    break;
+                case"warn":
+                    mllog.Warning(thingtolog);
+                    break;
+                 case"error":
+                    mllog.Error(thingtolog);
+                    break;
+            }
+        }
+        public override void OnApplicationStart(){
+            mllog=LoggerInstance;
+            Log("template loaded!");
+        }
+    }
+}
